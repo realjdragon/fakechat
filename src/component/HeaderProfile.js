3c0edf822c30fb172b76588fda46c0feda1ae8f7
jdragon <onerndj2@gmail.com>
2020-06-18 17:05:23 +0900

    editable

--- src/component/HeaderProfile.js
+++ src/component/HeaderProfile.js
@@ -3,13 +3,19 @@ import './HeaderProfile.scss';
 import profile from './images/profile.jpg';
 
 const HeaderProfile = () => {
+    const handleKeyPress = (e) => {
+      if (e.which === 13) {
+        e.preventDefault();
+      }
+    };
+
   return (
     <div className="HeaderProfile">
       <div className="profileImage">
         <img src={profile} alt="프로필사진" />
       </div>
       <div className="profileName">
-        James <span>></span>
+        <span className="typedName" contenteditable="true" onKeyPress={(e) => handleKeyPress(e)}>James</span> <span className="arrow">></span>
       </div>
     </div>
   );
