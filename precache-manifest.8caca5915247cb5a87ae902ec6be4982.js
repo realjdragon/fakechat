a6f914aa04694fb72e6a26dc6b172ca277cd23ac
jdragon <onerndj2@gmail.com>
2020-06-18 17:58:08 +0900

    Updates

---
+++ precache-manifest.8caca5915247cb5a87ae902ec6be4982.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feff15c29f5c52f2ad029070f53076e8",
+    "url": "/fakechat/index.html"
+  },
+  {
+    "revision": "02165631c6b3cce23d1c",
+    "url": "/fakechat/static/css/main.6c38a7de.chunk.css"
+  },
+  {
+    "revision": "4fa822df1ca1bfeab3a1",
+    "url": "/fakechat/static/js/2.2cceb59d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fakechat/static/js/2.2cceb59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02165631c6b3cce23d1c",
+    "url": "/fakechat/static/js/main.ed852db9.chunk.js"
+  },
+  {
+    "revision": "0c76eb9ebed3440666ba",
+    "url": "/fakechat/static/js/runtime-main.02ddb5ea.js"
+  },
+  {
+    "revision": "f85df2f16daa9bada23f51839d89f840",
+    "url": "/fakechat/static/media/profile.f85df2f1.jpg"
+  }
+]);
